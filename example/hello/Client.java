4ae47b26670eb512fffac08725dc666cb0c30fce
EduardoBottosso <eduardobottosso@discente.ufg.br>
2026-04-12 22:10:56 -0300

    Update Client.java

--- example/hello/Client.java
+++ example/hello/Client.java
@@ -1,9 +1,6 @@
 package example.hello;
 
 import java.rmi.Naming;
-import java.rmi.RemoteException;
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
 
 public class Client {
 
@@ -13,26 +10,33 @@ public class Client {
 
         System.out.println("Initiating client");
         
-        String host = (args.length < 1) ? null : args[0];
-        try {
-            //Registry registry = LocateRegistry.getRegistry(host);
-            //System.out.println("Registry has been located");
-            //Hello stub = (Hello) registry.lookup("Hello");
+        String host = (args.length < 1) ? "localhost" : args[0];
 
-            Hello stub = (Hello) Naming.lookup("rmi://" + host + "/MyHello"); 
-            System.out.println("Found server");
+        try {
+            Hello stub = (Hello) Naming.lookup("rmi://" + host + "/MyHello");
+            System.out.println("Found Hello server");
+
             String response = stub.sayHello();
             System.out.println("Response: " + response);
 
-            int result = stub.soma(100,1000);
+            int result = stub.soma(100, 1000);
             System.out.println("Response from soma: " + result);
 
-            int multi = stub.multiplicacao(100,1000);
+            int multi = stub.multiplicacao(100, 1000);
             System.out.println("Response from multiplicacao: " + multi);
 
             int quad = stub.quadrado(100);
-            System.out.println("Responde from quadrado: " + quad);
-            
+            System.out.println("Response from quadrado: " + quad);
+
+            Calculator calc = (Calculator) Naming.lookup("rmi://" + host + "/MyCalculator");
+            System.out.println("Found Calculator server");
+
+            int calcMulti = calc.multiplicacao(10, 5);
+            System.out.println("Calc multiplicacao: " + calcMulti);
+
+            int calcDiv = calc.divisao(20, 4);
+            System.out.println("Calc divisao: " + calcDiv);
+
         } catch (Exception e) {
             System.err.println("Client exception: " + e.toString());
             e.printStackTrace();
